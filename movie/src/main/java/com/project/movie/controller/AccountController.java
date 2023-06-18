39d30b81e50ff402cbe8e5ae2f8abf2cda474ce9
wtc <839385081@qq.com>
2023-06-18 10:57:06 +0800

    AccountController

--- movie/src/main/java/com/project/movie/controller/AccountController.java
+++ movie/src/main/java/com/project/movie/controller/AccountController.java
@@ -1,4 +1,24 @@
 package com.project.movie.controller;
 
+import com.project.movie.domain.AjaxResult;
+import com.project.movie.domain.VO.UserLoginVO;
+import com.project.movie.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/account")
 public class AccountController {
+    @Autowired
+    private AccountService accountService;
+
+    @PostMapping("/login")
+    public AjaxResult login(@RequestParam String email, @RequestParam String password) {
+        return accountService.login(email, password);
+    }
+
+    @PostMapping("/register")
+    public AjaxResult register(@RequestBody UserLoginVO vo) {
+        return accountService.register(vo);
+    }
 }
