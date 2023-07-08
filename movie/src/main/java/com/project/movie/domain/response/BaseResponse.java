03cc4754eeb284f4ea85e16ac500bf43736a3c1c
celiaf <feng598164096@qq.com>
2023-07-08 16:49:10 +0800

    rollback

--- movie/src/main/java/com/project/movie/domain/response/BaseResponse.java
+++ movie/src/main/java/com/project/movie/domain/response/BaseResponse.java
@@ -2,10 +2,6 @@ package com.project.movie.domain.response;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
-import org.springframework.http.HttpStatus;
-
-import java.io.Serial;
-import java.util.HashMap;
 
 @Data
 @Accessors(chain = true)
