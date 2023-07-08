bdda11de241db58b31da021a86f48601bb16a7a6
wtc <839385081@qq.com>
2023-07-09 02:40:03 +0800

    fix: remove target

--- movie/target/classes/Script/DropDuplicates.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-path_credits = "/Users/jackwu/Desktop/HKU/Project/credits.csv"
-path_metadata = "/Users/jackwu/Desktop/HKU/Project/movies_metadata.csv"
-
-data = pd.read_csv(path_credits, encoding="latin1")
-data.drop_duplicates(subset=['id'], keep='first', inplace=True)
-data.to_csv(path_credits, encoding="latin1")
