644475d6c270a715c29d213764241b34e8b39b8a
wtc <839385081@qq.com>
2023-07-13 01:09:30 +0800

    feat: ItemRecommder override

--- movie/src/main/java/com/project/movie/service/recommend/impl/ItemRecommender.java
+++ movie/src/main/java/com/project/movie/service/recommend/impl/ItemRecommender.java
@@ -2,6 +2,11 @@ package com.project.movie.service.recommend.impl;
 
 import com.project.movie.service.recommend.AbsRecommender;
 
-public class ItemRecommender extends AbsRecommender {
+import java.util.List;
 
+public class ItemRecommender extends AbsRecommender {
+	@Override
+	protected List<Object> recall(Integer userId) {
+		return null;
+	}
 }
